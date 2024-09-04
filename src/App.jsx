2bcf96dc06e3f54fdbd7b4634bd8e1e69cae213d
PrathamSharma-2811 <prathamnov@gmail.com>
2024-09-04 15:47:50 +0530

    4th septemeber Made changes in dashboard added themes and charts

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import Sidebar from './components/Sidebar'
 import Navbar from './components/Navbar'
 import Dashboard from './components/Dashboard'
+import ThemeContextProvider from './context/ThemeContextProvider'
+
 
 
 const App = () => {
   return (
+	<ThemeContextProvider>
 	<div className='flex '>
 		<Sidebar/>
 		<div className='grow ml-16 md:ml-64 h-full lg:h-screen'>
@@ -15,8 +18,8 @@ const App = () => {
 		<Dashboard/>
 		</div>
 		</div>
-		
-	</div>
+    </div>
+    </ThemeContextProvider>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { FaMoon, FaSun } from 'react-icons/fa'
+import { ThemeCotext } from '../context/ThemeContextProvider'
 
 const Navbar = () => {
+    const {theme, toggleTheme} = useContext(ThemeCotext)
   return (
-    <div className='bg-slate-600 border-b text-white border-b-black shadow-xl items-center p-5 flex '>
-      <h1>Dashboard</h1>
+    <div className='bg-gray-100 text-gray-900 border-b border-gray-300 p-4 flex justify-between items-center dark:border-gray-600 dark:bg-gray-900 dark:text-white'>
+        <h1>Dashboard</h1>
+        <button className='text-2xl text-dark' onClick={toggleTheme}>
+            {theme === "light" ? <FaMoon /> : <FaSun />}
+        </button>
     </div>
   )
 }
